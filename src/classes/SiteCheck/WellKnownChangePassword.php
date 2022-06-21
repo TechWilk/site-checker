7be4963aa3e2cfb176e2eef9d87c4a79be573c40
Christopher Wilkinson <c@wilk.tech>
2022-06-21 20:37:18 +0100

    Make change-password check stricter to reduce false positive

--- src/classes/SiteCheck/WellKnownChangePassword.php
+++ src/classes/SiteCheck/WellKnownChangePassword.php
@@ -11,6 +11,7 @@ class WellKnownChangePassword implements SiteCheck
 {
     protected $tests = [
         'redirect_exists',
+        'page_exists_at_end_of_chain',
     ];
 
     public function run(string $siteUrl): array
@@ -22,8 +23,21 @@ class WellKnownChangePassword implements SiteCheck
             'http_errors' => false, // don't throw 404s as exceptions
         ]);
 
+        if (!in_array($response->getStatusCode(), [301, 302])) {
+            return [
+                'redirect_exists' => in_array($response->getStatusCode(), [301, 302]),
+                'page_exists_at_end_of_chain' => null,
+            ];
+        }
+
+        $responseEndOfChain = $client->get('/.well-known/change-password', [
+            'allow_redirects' => true,
+            'http_errors' => false, // don't throw 404s as exceptions
+        ]);
+
         return [
             'redirect_exists' => in_array($response->getStatusCode(), [301, 302]),
+            'page_exists_at_end_of_chain' => !in_array($responseEndOfChain->getStatusCode(), [404]),
         ];
     }
 }
